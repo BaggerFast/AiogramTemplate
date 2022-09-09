6218740b9d08ff704898c8e2a468e68fa33110d9
baggerfast <riosha3@gmail.com>
2022-09-10 02:36:34 +0300

    ADD: filters package

---
+++ bot/filters/main.py
@@ -0,0 +1,6 @@
+from aiogram import Dispatcher
+
+
+def register_all_filters(dp: Dispatcher):
+    # todo: register all filters - dp.bind_filter()
+    pass
--- bot/main.py
+++ bot/main.py
@@ -2,12 +2,14 @@ from aiogram.utils import executor
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+from bot.filters import register_all_filters
 from bot.misc import TgKeys
 from bot.handlers import register_all_handlers
 from bot.database.models import register_models
 
 
 async def __on_start_up(dp: Dispatcher) -> None:
+    register_all_filters(dp)
     register_all_handlers(dp)
     register_models()
 
